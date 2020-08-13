b76fa53b1c55f0115effb64e98f31d51d29e40e9
abbuyanaa <abbuyanaa@gmail.com>
2020-08-13 17:54:31 +0800

    Finance module ruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -23,18 +23,19 @@ var uiController = (function () {
 
 // Санхүүтэй харьцах контроллер
 var financeController = (function () {
+  // private data
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  // private data
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-
+  // private data
   var data = {
     allItems: {
       inc: [],
@@ -45,14 +46,39 @@ var financeController = (function () {
       exp: 0,
     },
   };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.item[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.item[type].push(item);
+    },
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 
 // Програмын холбогч контроллер
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцээр олж авна.
-    console.log(uiController.getInput().value);
+    var input = uiController.getInput();
+
+    console.log(input);
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөгдлүүдээ веб дээрэй тохирох хэсэгт нь гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
